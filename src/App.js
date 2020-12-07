7e0371a3c7601b12dd09fb362bec0637bf836425
Aleks Maskimov <sanyamaks14@gmail.com>
2020-12-08 00:32:29 +0400

    Add styles for Header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import News from './components/News/News';
+import Background from "./components/Background/Background";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Background />
       <News />
     </div>
   );
