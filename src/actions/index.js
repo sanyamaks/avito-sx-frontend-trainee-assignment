1f2bd6cdfd1e4186e917e165618ac37f1d162dc8
Aleks Maskimov <sanyamaks14@gmail.com>
2020-12-07 21:22:24 +0400

    Add sort and transfer date for news

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,21 @@ export const requestNews = () => {
   return (dispatch) => {
     dispatch(setLoading(true));
     getAllNewsId()
+      .then((news) =>
+        news.sort((a, b) => {
+          return b.time - a.time;
+        })
+      )
+      .then((news) => {
+        console.log(news);
+        return news.map((item) => {
+          const date = new Date(item.time * 1000);
+          item.time = `${date.getDate()}.${
+            date.getMonth() + 1
+          }.${date.getFullYear()}`;
+          return item;
+        });
+      })
       .then((news) => dispatch(addNews(news)))
       .finally(() => dispatch(setLoading(false)));
   };
