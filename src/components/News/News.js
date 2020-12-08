66833af060720d918fe40237d9311f0cdc270ebd
Aleksandr Maksimov <sanyamaks14@gmail.com>
2020-12-08 09:50:33 +0400

    Add re-render News

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NewsItem from '../NewsItem/NewsItem';
 import { connect } from 'react-redux';
 import { requestNews } from '../../store/actions/index';
@@ -6,14 +6,26 @@ import './News.css';
 
 const News = (props) => {
   const { news, showNews } = props;
+  const [count, setCount] = useState(0);
+
+  const tick = () => {
+    setCount(count + 1);
+  };
 
   useEffect(() => {
     showNews();
-  }, []);
+    const interval = setInterval(tick, 1000 * 60);
+
+    return () => clearInterval(interval);
+  }, [count]);
   return (
     <div className="news">
       {news.map((newsItem) => (
-        <NewsItem className="news__news-item" newsItem={newsItem} key={newsItem.id} />
+        <NewsItem
+          className="news__news-item"
+          newsItem={newsItem}
+          key={newsItem.id}
+        />
       ))}
     </div>
   );
