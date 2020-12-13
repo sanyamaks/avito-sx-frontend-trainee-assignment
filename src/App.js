465d63a1ad47c49110d211f666cf0a2bbc41e678
Aleksandr Maksimov <sanyamaks14@gmail.com>
2020-12-14 02:21:51 +0400

    Replaced BrowserRouter with HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, useParams } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import MainPage from './components/MainPage/MainPage';
