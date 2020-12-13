395d9549f53a0b1d88e3d3b87dc5dce772a37b8d
Aleksandr Maksimov <sanyamaks14@gmail.com>
2020-12-14 02:36:11 +0400

    Fix timeConverter and styles of time

--- src/utils/timeConverter.js
+++ src/utils/timeConverter.js
@@ -1,5 +1,9 @@
 export const convertTime = (obj) => {
   const date = new Date(obj.time * 1000);
-  obj.time = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+  obj.time = `${date.getDate()}.${
+    date.getMonth() + 1
+  }.${date.getFullYear()}, ${date.getHours()}:${
+    date.getMinutes() < 10 ? '0' : ''
+  }${date.getMinutes()}`;
   return obj;
 };
